934fdcf30a60356eff42b7b660defffa8936e0b9
Cabbageroll775 <24-06925@g.batstate-u.edu.ph>
2025-12-04 22:26:46 +0800

    Add files via upload

--- FASTAF(AS FINGERS)/MusicPlayerLoop.java
+++ FASTAF(AS FINGERS)/MusicPlayerLoop.java
@@ -34,23 +34,20 @@ public class MusicPlayerLoop {
     public static void musicPicker() {
         java.util.Scanner scanner = new java.util.Scanner(System.in);
         System.out.println("🎵 MUSIC PICKER 🎵");
-        System.out.println("1. Bad Blood");
-        System.out.println("2. Dance Of Swords");
-        System.out.println("3. Game Corner");
-        System.out.println("4. Nimbasa");
-        System.out.println("5. Getaway Car");
-        System.out.println("6. Watermelon Battle");
+        System.out.println("1. Watermelon Battle");
+        System.out.println("2. Sardonyxs");
+        System.out.println("3. Almost There");
+        System.out.println("4. Watermelon Steven");
+
         System.out.print("Enter choice: ");
         int choice = scanner.nextInt();
         scanner.nextLine();
 
         switch (choice) {
-            case 1 -> playMusic("Bad Blood.wav");
-            case 2 -> playMusic("Dance Of Swords.wav");
-            case 3 -> playMusic("Game Corner.wav");
-            case 4 -> playMusic("Nimbasa.wav");
-            case 5 -> playMusic("Getaway Car.wav");
-            case 6 -> playMusic("Watermelon Battle.wav");
+            case 1 -> playMusic("Watermelon Battle.wav");
+            case 2 -> playMusic("Sardonyxs Theme.wav");
+            case 3 -> playMusic("Almost There.wav");
+            case 4 -> playMusic("Watermelon Steven.wav");
             default -> System.out.println("Invalid choice!");
         }
     }
